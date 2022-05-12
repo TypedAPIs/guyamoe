62acde212d6e8ea5f526afb5be7112ea1d8a2299
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-05-11 22:26:55 -0400

    Add search index

---
+++ python/docs/endpoints/search_index.rst
@@ -0,0 +1,9 @@
+Search Index
+------------
+
+Endpoint: ``/search_index/{slug}``
+
+
+.. automodule:: guyamoe_api_types.search_index
+    :members:
+    :show-inheritance:
--- python/src/guyamoe_api_types/__init__.py
+++ python/src/guyamoe_api_types/__init__.py
@@ -1,5 +1,6 @@
 from .all_groups import AllGroups
 from .all_series import AllSeries, SeriesSummary
+from .search_index import SearchIndex
 from .series import Chapter, Series
 from .series_groups import SeriesGroups
 from .series_page_data import SeriesPageData
