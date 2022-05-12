2fec7fc8cccebe643aa7cc5a92c4d9f513b99a83
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-05-11 21:45:01 -0400

    Add series volume cover

--- python/src/guyamoe_api_types/__init__.py
+++ python/src/guyamoe_api_types/__init__.py
@@ -3,3 +3,4 @@ from .all_series import AllSeries, SeriesSummary
 from .series import Chapter, Series
 from .series_groups import SeriesGroups
 from .series_page_data import SeriesPageData
+from .series_volume_cover import SeriesVolumeCover
